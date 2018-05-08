38fd687258d0473129cb4eac3b03ef08734229a1
Unknown <wndtanaka@gmail.com>
2018-05-08 12:01:47 +1000

    added pieces, code whrn you sit the throne. not finished

--- Assets/Resources/Scripts/CameraRaycaster.cs
+++ Assets/Resources/Scripts/CameraRaycaster.cs
@@ -2,79 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraRaycaster : MonoBehaviour
+namespace UnityStandardAssets.Characters.FirstPerson
 {
-    [SerializeField]
-    float rayRange;
-    [SerializeField]
-    LayerMask layerMask;
-    [SerializeField]
-    Vector3 YOffset = new Vector3(0, 1.5f, 0);
-
-    bool isOpen = false;
-
-
-    void Start()
+    public class CameraRaycaster : MonoBehaviour
     {
+        [SerializeField]
+        float rayRange = 2;
+        [SerializeField]
+        LayerMask layerMask;
+        [SerializeField]
+        Vector3 YOffset = new Vector3(0, 1.5f, 0);
 
-    }
+        bool isOpen = false;
+        bool isSeated = false;
 
-    void Update()
-    {
-        // raycast from the player
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, rayRange, layerMask))
+        RigidbodyFirstPersonController controller;
+        [SerializeField]
+        MouseLook mouseLook = new MouseLook();
+
+        void Start()
         {
-            // Open and close door mechanics, door able to open outwards depends on player position
-            if (Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Door"))
+            controller = GetComponent<RigidbodyFirstPersonController>();
+            mouseLook.Init(transform, Camera.main.transform);
+        }
+
+        void Update()
+        {
+            if (isSeated)
             {
-                RaycastHit inHit;
-                RaycastHit outHit;
-                RaycastHit closeHit;
-
-                // The rotation will affect 'Hinge' GameObject
-                // get 'Hinge' transform component because 'Hinge' gameobject is the pivot point
-                Transform hinge = hit.transform.parent.GetComponent<Transform>();
-
-                // if the door is closed draw ray from both side of the door to locate player position
-                if (!isOpen)
+                mouseLook.ClampRotation(transform, Camera.main.transform);
+            }
+            // raycast from the player
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, rayRange, layerMask))
+            {
+                #region Door Raycaster
+                // Open and close door mechanics, door able to open outwards depends on player position
+                if (Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Door"))
                 {
-                    if (Physics.Raycast(hit.transform.position - new Vector3(-0.2f, YOffset.y, 0), transform.position, out inHit, rayRange + 100))
-                    {
-                        Debug.DrawRay(hit.transform.position - new Vector3(-0.2f, YOffset.y, 0), transform.position, Color.red);
+                    RaycastHit inHit;
+                    RaycastHit outHit;
+                    RaycastHit closeHit;
 
-                        if (inHit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                    // The rotation will affect 'Hinge' GameObject
+                    // get 'Hinge' transform component because 'Hinge' gameobject is the pivot point
+                    Transform hinge = hit.transform.parent.GetComponent<Transform>();
+
+                    // if the door is closed draw ray from both side of the door to locate player position
+                    if (!isOpen)
+                    {
+                        if (Physics.Raycast(hit.transform.position - new Vector3(-0.2f, YOffset.y, 0), transform.position, out inHit, rayRange + 100))
                         {
-                            hinge.Rotate(0, 90, 0);
-                            Debug.Log("Open");
-                            isOpen = !isOpen;
-                            // TODO Animation for the door
+                            Debug.DrawRay(hit.transform.position - new Vector3(-0.2f, YOffset.y, 0), transform.position, Color.red);
+
+                            if (inHit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                            {
+                                hinge.Rotate(0, 90, 0);
+                                Debug.Log("Open");
+                                isOpen = !isOpen;
+                                // TODO Animation for the door
+                            }
+                        }
+                        if (Physics.Raycast(hit.transform.position - new Vector3(0.2f, YOffset.y, 0), -transform.position, out outHit, rayRange + 100))
+                        {
+                            Debug.DrawRay(hit.transform.position - new Vector3(0.2f, YOffset.y, 0), -transform.position, Color.blue);
+                            if (outHit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                            {
+                                hinge.Rotate(0, -90, 0);
+                                Debug.Log("Open");
+                                isOpen = !isOpen;
+                                // TODO Animation for the door
+                            }
                         }
                     }
-                    if (Physics.Raycast(hit.transform.position - new Vector3(0.2f, YOffset.y, 0), -transform.position, out outHit, rayRange + 100))
+                    // if the door is open
+                    else
                     {
-                        Debug.DrawRay(hit.transform.position - new Vector3(0.2f, YOffset.y, 0), -transform.position, Color.blue);
-                        if (outHit.transform.gameObject.layer == LayerMask.NameToLayer("Player"))
+                        if (Physics.Raycast(hit.transform.position - new Vector3(0.5f, YOffset.y, 0), transform.forward, out closeHit, rayRange + 100))
                         {
-                            hinge.Rotate(0, -90, 0);
-                            Debug.Log("Open");
+                            Debug.DrawRay(hit.transform.position - new Vector3(0.5f, YOffset.y, 0), transform.forward, Color.black);
+                            // set rotation position to normal / default
+                            hinge.rotation = Quaternion.Euler(0, 0, 0);
+                            Debug.Log("Closed");
                             isOpen = !isOpen;
                             // TODO Animation for the door
                         }
                     }
                 }
-                // if the door is open
-                else
+                #endregion
+
+                if (Input.GetKeyDown(KeyCode.E) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Throne"))
                 {
-                    if (Physics.Raycast(hit.transform.position - new Vector3(0.5f, YOffset.y, 0), transform.forward, out closeHit, rayRange + 100))
-                    {
-                        Debug.DrawRay(hit.transform.position - new Vector3(0.5f, YOffset.y, 0), transform.forward, Color.black);
-                        // set rotation position to normal / default
-                        hinge.rotation = Quaternion.Euler(0, 0, 0);
-                        Debug.Log("Closed");
-                        isOpen = !isOpen;
-                        // TODO Animation for the door
-                    }
+                    Debug.Log("Sit");
+                    // TODO sit on the throne
+                    transform.position = hit.transform.position + new Vector3(1, 0, 0);
+                    controller.enabled = false;
+                    transform.rotation = Quaternion.Euler(0, 90, 0);
+                    isSeated = true;
                 }
             }
         }
--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -12,6 +12,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         public bool clampVerticalRotation = true;
         public float MinimumX = -90F;
         public float MaximumX = 90F;
+        public float MinimumY = -90F;
+        public float MaximumY = 90f;
         public bool smooth;
         public float smoothTime = 5f;
         public bool lockCursor = true;
@@ -109,6 +111,51 @@ namespace UnityStandardAssets.Characters.FirstPerson
             q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
 
             return q;
+        }
+
+        Quaternion ClampRotationAroundYAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            float angleY = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+            angleY = Mathf.Clamp(angleY, MinimumY, MaximumY);
+
+            q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleY);
+
+            return q;
+        }
+
+        public void ClampRotation(Transform character, Transform camera)
+        {
+            float yRot = CrossPlatformInputManager.GetAxis("Mouse X") * XSensitivity;
+            float xRot = CrossPlatformInputManager.GetAxis("Mouse Y") * YSensitivity;
+
+            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+            if (clampVerticalRotation)
+            {
+                m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+                // TODO clamp
+            }
+            if (smooth)
+            {
+                character.localRotation = Quaternion.Slerp(character.localRotation, m_CharacterTargetRot,
+                    smoothTime * Time.deltaTime);
+                camera.localRotation = Quaternion.Slerp(camera.localRotation, m_CameraTargetRot,
+                    smoothTime * Time.deltaTime);
+            }
+            else
+            {
+                character.localRotation = m_CharacterTargetRot;
+                camera.localRotation = m_CameraTargetRot;
+            }
+
+            UpdateCursorLock();
         }
 
     }
